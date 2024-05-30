8a9183576e205e113c865ae3d7a24819e169d67b
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2024-05-30 14:49:27 +0900

    fix bug not matches

--- branch_count.py
+++ branch_count.py
@@ -65,9 +65,15 @@ def count_branches(node, slf4j_loggers):
     if_branches = 0
     else_branches = 0
     switch_cases = 0
+    try_blocks = 0
+    catch_blocks = 0
+    finally_blocks = 0
     if_branches_with_logs = 0
     else_branches_with_logs = 0
     switch_cases_with_logs = 0
+    try_blocks_with_logs = 0
+    catch_blocks_with_logs = 0
+    finally_blocks_with_logs = 0
     log_positions = []
 
     if isinstance(node, javalang.tree.IfStatement):
@@ -100,37 +106,76 @@ def count_branches(node, slf4j_loggers):
             for stmt in node.statements:
                 log_positions.extend(find_logging_positions(stmt, slf4j_loggers))
 
+    if isinstance(node, javalang.tree.TryStatement):
+        try_blocks += 1
+        if any(contains_slf4j_logging(stmt, slf4j_loggers) for stmt in node.block):
+            try_blocks_with_logs += 1
+            for stmt in node.block:
+                log_positions.extend(find_logging_positions(stmt, slf4j_loggers))
+        if node.catches:
+            for catch_clause in node.catches:
+                catch_blocks += 1
+                if any(contains_slf4j_logging(stmt, slf4j_loggers) for stmt in catch_clause.block):
+                    catch_blocks_with_logs += 1
+                    for stmt in catch_clause.block:
+                        log_positions.extend(find_logging_positions(stmt, slf4j_loggers))
+                        
+        if node.finally_block:
+            finally_blocks += 1
+            if any(contains_slf4j_logging(stmt, slf4j_loggers) for stmt in node.finally_block):
+                finally_blocks_with_logs += 1
+                for stmt in node.finally_block:
+                    log_positions.extend(find_logging_positions(stmt, slf4j_loggers))
+
     if hasattr(node, 'children'):
         for child in node.children:
             if isinstance(child, list):
                 for sub_child in child:
-                    ib, eb, sb, ibwl, ebwl, sbwl, lp = count_branches(sub_child, slf4j_loggers)
+                    ib, eb, sb, tb, cb, fb, ibwl, ebwl, sbwl, tbwl, cbwl, fbwl, lp = count_branches(sub_child, slf4j_loggers)
                     if_branches += ib
                     else_branches += eb
                     switch_cases += sb
+                    try_blocks += tb
+                    catch_blocks += cb
+                    finally_blocks += fb
                     if_branches_with_logs += ibwl
                     else_branches_with_logs += ebwl
                     switch_cases_with_logs += sbwl
+                    try_blocks_with_logs += tbwl
+                    catch_blocks_with_logs += cbwl
+                    finally_blocks_with_logs += fbwl
                     log_positions.extend(lp)
             elif child:
-                ib, eb, sb, ibwl, ebwl, sbwl, lp = count_branches(child, slf4j_loggers)
+                ib, eb, sb, tb, cb, fb, ibwl, ebwl, sbwl, tbwl, cbwl, fbwl, lp = count_branches(child, slf4j_loggers)
                 if_branches += ib
                 else_branches += eb
                 switch_cases += sb
+                try_blocks += tb
+                catch_blocks += cb
                 if_branches_with_logs += ibwl
                 else_branches_with_logs += ebwl
                 switch_cases_with_logs += sbwl
+                try_blocks_with_logs += tbwl
+                catch_blocks_with_logs += cbwl
                 log_positions.extend(lp)
 
-    return if_branches, else_branches, switch_cases, if_branches_with_logs, else_branches_with_logs, switch_cases_with_logs, log_positions
+    return (if_branches, else_branches, switch_cases, try_blocks, catch_blocks, finally_blocks,
+            if_branches_with_logs, else_branches_with_logs, switch_cases_with_logs,
+            try_blocks_with_logs, catch_blocks_with_logs, finally_blocks_with_logs, log_positions)
 
 def parse_java_files(directory):
     if_branches = 0
     else_branches = 0
     switch_cases = 0
+    try_blocks = 0
+    catch_blocks = 0
+    finally_blocks = 0
     if_branches_with_logs = 0
     else_branches_with_logs = 0
     switch_cases_with_logs = 0
+    try_blocks_with_logs = 0
+    catch_blocks_with_logs = 0
+    finally_blocks_with_logs = 0
     branches_with_log_positions = []
     for root, _, files in os.walk(directory):
         for file in files:
@@ -145,86 +190,113 @@ def parse_java_files(directory):
                             for path, node in tree.filter(javalang.tree.VariableDeclarator):
                                 collect_slf4j_loggers(node, slf4j_loggers)
                         for path, node in tree:
-                            ib, eb, sb, ibwl, ebwl, sbwl, lp = count_branches(node, slf4j_loggers)
+                            ib, eb, sb, tb, cb, fb, ibwl, ebwl, sbwl, tbwl, cbwl, fbwl, lp = count_branches(node, slf4j_loggers)
                             if_branches += ib
                             else_branches += eb
                             switch_cases += sb
+                            try_blocks += tb
+                            catch_blocks += cb
+                            finally_blocks += fb
                             if_branches_with_logs += ibwl
                             else_branches_with_logs += ebwl
                             switch_cases_with_logs += sbwl
+                            try_blocks_with_logs += tbwl
+                            catch_blocks_with_logs += cbwl
+                            finally_blocks_with_logs += fbwl
                             for pos in lp:
                                 branches_with_log_positions.append((file, pos))
                     except (javalang.parser.JavaSyntaxError, IndexError) as e:
                         print(f"Error parsing {file_path}: {e}")
 
-    return if_branches, else_branches, switch_cases, if_branches_with_logs, else_branches_with_logs, switch_cases_with_logs, branches_with_log_positions
+    return (if_branches, else_branches, switch_cases, try_blocks, catch_blocks, finally_blocks,
+            if_branches_with_logs, else_branches_with_logs, switch_cases_with_logs,
+            try_blocks_with_logs, catch_blocks_with_logs, finally_blocks_with_logs, branches_with_log_positions)
+    
+def report_number(topics):
+    for topic in topics:
+        try:
+            print(f"Number of {topic} with SLF4J logging: {topics[topic][1]}/{topics[topic][0]}")
+        except ZeroDivisionError:
+            print(f"Number of {topic} with SLF4J logging: 0/0")
 
-def check_log_coverage(branches_with_log_positions, covered_lines):
-    covered_log_branches = 0
-    for file, line in branches_with_log_positions:
-        if (file, line) in covered_lines:
-            covered_log_branches += 1
-    return covered_log_branches
+def report_percentage(topics):
+    for topic in topics:
+        try:
+            print(f"Percentage of {topic} with SLF4J logging: {topics[topic][1]/topics[topic][0]*100:.2f}%")
+        except ZeroDivisionError:
+            print(f"Percentage of {topic} with SLF4J logging: 0.00%")
 
-# Define the root directory containing all the project directories
-root_directory = '.'
+def analyze_repository(repository_path):
+    total_if_branches = 0
+    total_else_branches = 0
+    total_switch_cases = 0
+    total_try_blocks = 0
+    total_catch_blocks = 0
+    total_finally_blocks = 0
+    total_if_branches_with_logs = 0
+    total_else_branches_with_logs = 0
+    total_switch_cases_with_logs = 0
+    total_try_blocks_with_logs = 0
+    total_catch_blocks_with_logs = 0
+    total_finally_blocks_with_logs = 0
+    total_log_positions = []
 
-jacoco_reports = find_jacoco_reports(root_directory)
-all_covered_lines = set()
+    jacoco_reports = find_jacoco_reports(repository_path)
+    covered_lines = set()
+    for report in jacoco_reports:
+        covered_lines.update(parse_jacoco_report(report))
 
-for report in jacoco_reports:
-    covered_lines = parse_jacoco_report(report)
-    all_covered_lines.update(covered_lines)
+    java_dirs = [d for d in os.listdir(repository_path) if os.path.isdir(os.path.join(repository_path, d))]
+    for java_dir in java_dirs:
+        full_java_dir = os.path.join(repository_path, java_dir)
+        if_branches, else_branches, switch_cases, try_blocks, catch_blocks, finally_block, if_branches_with_logs, else_branches_with_logs, switch_cases_with_logs, try_blocks_with_logs, catch_blocks_with_logs, finally_blocks_with_logs, log_positions = parse_java_files(full_java_dir)
 
-if_branches = 0
-else_branches = 0
-switch_cases = 0
-if_branches_with_logs = 0
-else_branches_with_logs = 0
-switch_cases_with_logs = 0
-branches_with_log_positions = []
+        total_if_branches += if_branches
+        total_else_branches += else_branches
+        total_switch_cases += switch_cases
+        total_try_blocks += try_blocks
+        total_catch_blocks += catch_blocks
+        total_finally_blocks += finally_block
+        total_if_branches_with_logs += if_branches_with_logs
+        total_else_branches_with_logs += else_branches_with_logs
+        total_switch_cases_with_logs += switch_cases_with_logs
+        total_try_blocks_with_logs += try_blocks_with_logs
+        total_catch_blocks_with_logs += catch_blocks_with_logs
+        total_finally_blocks_with_logs += finally_blocks_with_logs
+        total_log_positions.extend(log_positions)
 
-for root, dirs, files in os.walk(root_directory):
-    if 'pom.xml' in files:
-        java_dir = os.path.join(root, 'src')
-        if os.path.exists(java_dir):
-            ib, eb, sb, ibwl, ebwl, sbwl, log_positions = parse_java_files(java_dir)
-            if_branches += ib
-            else_branches += eb
-            switch_cases += sb
-            if_branches_with_logs += ibwl
-            else_branches_with_logs += ebwl
-            switch_cases_with_logs += sbwl
-            branches_with_log_positions.extend(log_positions)
+    total_branches = total_if_branches + total_else_branches + total_switch_cases + total_try_blocks + total_catch_blocks + total_finally_blocks
+    branches_with_logs = total_if_branches_with_logs + total_else_branches_with_logs + total_switch_cases_with_logs + total_try_blocks_with_logs + total_catch_blocks_with_logs + total_finally_blocks_with_logs
 
-print(branches_with_log_positions)
+    covered_log_branches = sum(1 for file, line in total_log_positions if (file, line) in covered_lines)
 
-covered_log_branches = check_log_coverage(branches_with_log_positions, all_covered_lines)
-total_log_branches = len(branches_with_log_positions)
+    if total_branches > 0:
+        percent_with_logs = (branches_with_logs / total_branches) * 100
+    else:
+        percent_with_logs = 0
 
-if total_log_branches > 0:
-    percent_covered = (covered_log_branches / total_log_branches) * 100
-else:
-    percent_covered = 0
+    if branches_with_logs > 0:
+        percent_covered = (covered_log_branches / branches_with_logs) * 100
+    else:
+        percent_covered = 0
+        
+    topics = {'if branches': (total_if_branches, total_if_branches_with_logs),
+              'else branches': (total_else_branches, total_else_branches_with_logs),
+              'switch cases': (total_switch_cases, total_switch_cases_with_logs),
+              'try blocks': (total_try_blocks, total_try_blocks_with_logs),
+              'catch blocks': (total_catch_blocks, total_catch_blocks_with_logs),
+              'finally blocks': (total_finally_blocks, total_finally_blocks_with_logs),
+              'total branches': (total_branches, branches_with_logs)}
 
-total_branches = if_branches + else_branches + switch_cases
-branches_with_logs = if_branches_with_logs + else_branches_with_logs + switch_cases_with_logs
+    
+    report_number(topics)
+    print('-'*60)
+    report_percentage(topics)
+    print('-'*60)
+    print(f"Total number of branches with SLF4J logging: {branches_with_logs}")
+    print(f"Number of branches with SLF4J logging covered by tests: {covered_log_branches}")
+    print(f"Percentage of branches with SLF4J logging covered by tests: {percent_covered:.2f}%")
 
-if total_branches > 0:
-    percent_with_logs = (branches_with_logs / total_branches) * 100
-else:
-    percent_with_logs = 0
-
-print(f"Number of if branches with SLF4J logging: {if_branches_with_logs}/{if_branches}")
-print(f"Number of else branches with SLF4J logging: {else_branches_with_logs}/{else_branches}")
-print(f"Number of switch cases with SLF4J logging: {switch_cases_with_logs}/{switch_cases}")
-print(f"Number of total branches with SLF4J logging: {branches_with_logs}/{total_branches}")
-print('-'*60)
-print(f"Percentage of if branches with SLF4J logging: {if_branches_with_logs/if_branches*100:.2f}%")
-print(f"Percentage of else branches with SLF4J logging: {else_branches_with_logs/else_branches*100:.2f}%")
-print(f"Percentage of switch cases with SLF4J logging: {switch_cases_with_logs/switch_cases*100:.2f}%")
-print(f"Percentage of branches with SLF4J logging: {percent_with_logs:.2f}%")
-print('-'*60)
-print(f"Total number of branches with SLF4J logging: {total_log_branches}")
-print(f"Number of branches with SLF4J logging covered by tests: {covered_log_branches}")
-print(f"Percentage of branches with SLF4J logging covered by tests: {percent_covered:.2f}%")
+# Specify the root directory of your project
+repository_path = './wro4j'
+analyze_repository(repository_path)
